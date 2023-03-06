da10e1386da893126d7561dfd7e94320e57eee14
paulg44 <paulgarton1984@hotmail.com>
2023-03-06 19:29:51 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function clock() {
+  const today = new Date();
+
+  let h = today.getHours();
+  let m = today.getMinutes();
+  m = checkTime(m);
+
+  document.getElementById('clock').innerHTML = h + ':' + m;
+}
+setInterval('clock()', 1000);
+
+function checkTime(i) {
+  if (i < 10) {
+    i = '0' + i;
+  }
+  return i;
+}
