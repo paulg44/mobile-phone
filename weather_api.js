47d4d3d7ebc8d25dfe5b7fd172744099d2eced5c
paulg44 <paulgarton1984@hotmail.com>
2023-07-22 21:33:10 +0100

    finish weather api and align in CSS

--- weather_api.js
+++ weather_api.js
@@ -10,7 +10,7 @@ async function fetchWeather() {
     const response = await fetch("http://api.weatherapi.com/v1/current.json?key=7c5aa96ac3444385920101530232207&q=derby");
     const data = await response.json();
     console.log(data);
-    temperature.textContent = data.current.temp_c;
+    temperature.textContent = `${data.current.temp_c}\u00B0C`
     console.log(temperature)
     const weatherImage = data.current.condition.icon;
     weatherIcon.src = `${weatherImage}`
