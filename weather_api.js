698abfe516cd3e26eaf151c84b8bfa605990424a
paulg44 <paulgarton1984@hotmail.com>
2023-08-14 21:05:00 +0100

    add the s to http and hopefully should call the api correctly on deployed site

--- weather_api.js
+++ weather_api.js
@@ -1,19 +1,21 @@
 // Import needed parts
-const temperature = document.getElementById('temp')
-const weatherIcon= document.getElementById('weather-icon')
+const temperature = document.getElementById('temp');
+const weatherIcon = document.getElementById('weather-icon');
 
 // API Key
 // key= <7c5aa96ac3444385920101530232207>
 
 // Call the weather API
 async function fetchWeather() {
-    const response = await fetch("http://api.weatherapi.com/v1/current.json?key=7c5aa96ac3444385920101530232207&q=derby");
-    const data = await response.json();
-    console.log(data);
-    temperature.textContent = `${data.current.temp_c}\u00B0C`
-    console.log(temperature)
-    const weatherImage = data.current.condition.icon;
-    weatherIcon.src = `${weatherImage}`
+  const response = await fetch(
+    'https://api.weatherapi.com/v1/current.json?key=7c5aa96ac3444385920101530232207&q=derby'
+  );
+  const data = await response.json();
+  console.log(data);
+  temperature.textContent = `${data.current.temp_c}\u00B0C`;
+  console.log(temperature);
+  const weatherImage = data.current.condition.icon;
+  weatherIcon.src = `${weatherImage}`;
 }
 
-fetchWeather()
+fetchWeather();
